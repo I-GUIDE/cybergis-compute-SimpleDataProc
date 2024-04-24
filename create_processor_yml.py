fdf6064926a83dd21c8399b408a390cdea303841
Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>
2024-04-24 14:13:38 -0400

    Update create_processor_yml.py

--- create_processor_yml.py
+++ create_processor_yml.py
@@ -3,8 +3,8 @@
 import yaml
 import os
 
-
-processor_dict ={'$1': {'SimpleDataProc':{'input_dir':os.environ['param_input_dir'], 'start_year': os.environ['param_start_year'],'end_year': os.environ['param_end_year']}}}
+input_dir = '/compute_scratch/%s' % os.environ['param_input_dir']
+processor_dict ={'$1': {'SimpleDataProc':{'input_dir':input_dir, 'start_year': os.environ['param_start_year'],'end_year': os.environ['param_end_year']}}}
 
 with open('/job/executable/data_process.yml','w') as demfile:
     yaml.dump(processor_dict,demfile)
